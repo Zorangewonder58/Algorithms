39e7ccde423351472e729ed2cb843a5dfb2d15f7
ashierap <112123390+ashierap@users.noreply.github.com>
2024-02-01 17:37:07 -0500

    Started function to create pseudorandom arrays (unfinished)

--- F1aLinearSearch-TRUSTY.cpp
+++ F1aLinearSearch-TRUSTY.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include <iomanip>
+#include <random>
 using namespace std;
 
 //ALGORITHM: ITERATIVE LINEAR SEARCH
 //TEAM: TRUSTY JADEN WILLIAMS ASHIERA PRESTON
 
+void show(string, int[], int);
+int itrLinearSearch(int[], int, int);
+void printSearch(int, int);
+int* pRand(int);
+
+int main()
+{
+    int test[20] = {67,22,95,11,56,78,33,89,44,5,70,14,99,38,73,20,81,50,7,65};
+    //int arr0[] = {};
+    //int* arr1 = pRand(1);
+    //int* arr1 = pRand(1);
+    int keys[10] = {67, 90, 25, 65, 63, 76, 7, 99, 100, 32};
+
+    show("\nTEST ARRAY", test, 20);
+    cout << endl;
+    show("KEYS", keys, 10);
+
+    cout << endl;
+
+    cout << endl << "\n==========RESULTS==========\n" << endl;
+
+    for(int i=0; i < 10; i++)
+    {
+        printSearch(itrLinearSearch(test, 20, keys[i]), keys[i]);
+    }
+
+    cout << endl << "(c) 2024 TRUSTY  Jaden Williams Ashiera Preston\n  ";
+
+    return 0;
+}
 
 //ARRAY PRINTING FUNCTION
 void show(string label, int array[], int size)
@@ -39,26 +70,15 @@ void printSearch(int position, int key)
         cout << setw(3) << key << " -- FOUND AT INDEX: " << position << endl;
 }
 
-
-int main()
-{
-    int test[20] = {67,22,95,11,56,78,33,89,44,5,70,14,99,38,73,20,81,50,7,65};
-    int keys[10] = {67, 90, 25, 65, 63, 76, 7, 99, 100, 32};
-
-    show("\nTEST ARRAY", test, 20);
-    cout << endl;
-    show("KEYS", keys, 10);
-
-    cout << endl;
-
-    cout << endl << "\n==========RESULTS==========\n" << endl;
-
-    for(int i=0; i < 10; i++)
-    {
-        printSearch(itrLinearSearch(test, 20, keys[i]), keys[i]);
+//PSEUDORANDOM ARRAY GENERATOR
+/*int* pRand(int size) {
+    random_device rd;
+    mt19937 gen(rd());
+    int* random_numbers = new int[size];
+    uniform_int_distribution<int> distribution(0, 100);
+    for (int i = 0; i < size; ++i) {
+        random_numbers[i] = distribution(gen);
     }
 
-    cout << endl << "(c) 2024 TRUSTY  Jaden Williams Ashiera Preston\n  ";
-
-    return 0;
-}
+    return random_numbers;
+}*/
