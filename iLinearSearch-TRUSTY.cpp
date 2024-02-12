76798a54a48eab185e22cc2a5c461f3762c447d5
ashierap <112123390+ashierap@users.noreply.github.com>
2024-02-11 22:56:57 -0500

    Fixed compare count
    Fixed the compare count in the code

--- iLinearSearch-TRUSTY.cpp
+++ iLinearSearch-TRUSTY.cpp
@@ -19,7 +19,7 @@ int main()
     //Array of size 0
     int arr0[0] = {};
 
-    //Array of size 2
+    //Array of size 1
     int arr1[1] = {14};
 
     //Array of size 2 
@@ -37,8 +37,7 @@ int main()
     //Array of size 128 
     int* arr128 = pRand(128);
 
-    //Initialize compare counter to zero
-    int compares = 0;
+    int compares;
 
     //Array of test cases
     int* cases[] = {arr0, arr1, arr2, arr8, arr16, arr64, arr128};
@@ -48,79 +47,81 @@ int main()
     show("\nSIZE 0", arr0, 0);
     show("\nSIZE 1", arr1, 1);
     show("\nSIZE 2", arr2, 2);
-    show("\nSIZE8", arr8, 8);
+    show("\nSIZE 8", arr8, 8);
     show("\nSIZE 16", arr16, 16);
     //show("\nSIZE 64", arr64, 64);
     //show("\nSIZE 128", arr128, 128);
 
     show("\nKEYS", keys, 10);
 
-    cout << endl << "\n==========Instrumented Search RESULTS==========\n" << endl;
+    cout << endl << "\n===================Instrumented Search RESULTS=================\n" << endl;
+
+    cout << endl << "\n=======================SEARCH CASE SIZE 0======================\n" << endl;
 
     //Search array of size 0
     for(int i=0; i < 10; i++)
     {
+        compares = 0;
         printInstrumentedSearch("iLinearSearch", keys[i], "Array0[0]", compares, iLinearSearch(arr0, 0, keys[i], compares));
         cout << endl;
     }
 
-    cout << endl << "\n==========SEARCH CASE SIZE 1==========\n" << endl;
-    compares = 0;
+    cout << endl << "\n=========================SEARCH CASE SIZE 1====================\n" << endl;
 
     //Search array of size 1
     for(int i=0; i < 10; i++)
     {
+        compares = 0;
         printInstrumentedSearch("iLinearSearch", keys[i], "Array1[1]", compares, iLinearSearch(arr1, 1, keys[i], compares));
         cout << endl;
     }
 
-     cout << endl << "\n==========SEARCH CASE SIZE 2==========\n" << endl;
-    compares = 0;
+    cout << endl << "\n=========================SEARCH CASE SIZE 2======================\n" << endl;
 
     //Search array of size 2
     for(int i=0; i < 10; i++)
     {
+        compares = 0;
         printInstrumentedSearch("iLinearSearch", keys[i], "Array2[2]", compares, iLinearSearch(arr2, 2, keys[i], compares));
         cout << endl;
     }
 
-     cout << endl << "\n==========SEARCH CASE SIZE 8==========\n" << endl;
-    compares = 0;
+    cout << endl << "\n=========================SEARCH CASE SIZE 8=======================\n" << endl;
 
     //Search array of size 8
     for(int i=0; i < 10; i++)
     {
+        compares = 0;
         printInstrumentedSearch("iLinearSearch", keys[i], "Array8[8]", compares, iLinearSearch(arr8, 8, keys[i], compares));
         cout << endl;
     }
 
-     cout << endl << "\n==========SEARCH CASE SIZE 16==========\n" << endl;
-    compares = 0;
+    cout << endl << "\n=========================SEARCH CASE SIZE 16=======================\n" << endl;
 
     //Search array of size 16
     for(int i=0; i < 10; i++)
     {
+        compares = 0;
         printInstrumentedSearch("iLinearSearch", keys[i], "Array16[16]", compares, iLinearSearch(arr16, 16, keys[i], compares));
         cout << endl;
     }
 
-   
-    cout << endl << "\n==========SEARCH CASE SIZE 64==========\n" << endl;
-    compares = 0;
+    cout << endl << "\n=========================SEARCH CASE SIZE 64========================\n" << endl;
 
-    //Search array of size 16
+    //Search array of size 64
     for(int i=0; i < 10; i++)
     {
+        compares = 0;
         printInstrumentedSearch("iLinearSearch", keys[i], "Array64[64]", compares, iLinearSearch(arr64, 64, keys[i], compares));
         cout << endl;
     }
 
-    cout << endl << "\n==========SEARCH CASE SIZE 128==========\n" << endl;
-    compares = 0;
+    cout << endl << "\n=========================SEARCH CASE SIZE 128=======================\n" << endl;
 
     //Search array of size 16
     for(int i=0; i < 10; i++)
     {
+        compares = 0;
         printInstrumentedSearch("iLinearSearch", keys[i], "Array128[128]", compares, iLinearSearch(arr128, 128, keys[i], compares));
         cout << endl;
     }
